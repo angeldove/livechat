02238f4ba1e97ef9f0242c9e56bdbbb9b87c66b4
angeldove <dev.roohiverma@gmail.com>
2016-05-10 20:59:15 +0530

    adding chat room model

--- chat/models.py
+++ chat/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    """
+    model for a chat room
+    """
+    name = models.CharField(max_length=64)
+    created_by = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+    
+    def __unicode__(self):
+        return self.name
