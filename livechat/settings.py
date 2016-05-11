e819fd978870c8848ac31c6c3e6dfa1657245995
angeldove <dev.roohiverma@gmail.com>
2016-05-11 18:16:59 +0530

    introducing registration app

--- livechat/settings.py
+++ livechat/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'chat.apps.ChatConfig',
+    'registration.apps.RegistrationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
