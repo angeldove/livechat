c0029d5f699a0b47d09491e47db93361aaf4ab47
angeldove <dev.roohiverma@gmail.com>
2016-05-12 19:25:40 +0530

    added model admins for room and message

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from chat.models import Room, Message
 
-admin.site.register(Room)
-admin.site.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'created_at')
+
+admin.site.register(Message, MessageAdmin)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_by', 'created_at')
+
+admin.site.register(Room, RoomAdmin)
