d3ea1f253b27bd46ade7769f9cacf7b3e566c318
angeldove <dev.roohiverma@gmail.com>
2016-05-13 13:20:24 +0530

    adding class for sending json responses via apis

--- chat/views.py
+++ chat/views.py
@@ -7,6 +7,17 @@ from django.http import *
 
 from livechat.forms import LoginForm
 
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
 def home(request):	
     return render_to_response('index.html')
 
